d2f556c195927ae42fb8bbf01c134d33056aec3c
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-05 22:57:15 +0000

    Updated buggy eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -9,17 +9,22 @@ const assertEqual = function(actual, expected) {
     }
   };
   
-const eqArrays = function([],[]){
-    for (let i = 0; i < eqArrays.length; i++){
-        if (eqArrays[0] === eqArrays[1]){
-              return true; 
-        }
-        if (eqArrays[0] !== eqArrays[1]){
-            return false;
-        }
-          }
+  const eqArrays = function(array1,array2) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
       }
-  
+    }
+    return true;
+  };
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
   // Test Code
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
