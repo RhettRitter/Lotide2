347e69339e870e670e132bdeaf55112eb6634c31
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-05 23:14:57 +0000

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,10 @@
+const assertArraysEqual = function(actual,expected) {
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🥵️Assertion Failed: ${actual} !== ${expected}`);
+    }
+  }
+  console.log(`🤑Assertion Passed: ${actual} === ${expected}`);
+};
+assertArraysEqual([1, 2, 3],[1, 2, 3]);
