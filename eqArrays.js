bee6763f8f9db07a61dbdb84290bf1ae3c3c2a41
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-11 15:44:10 +0000

    modularized eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,30 +1,16 @@
 // Function Implementation
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`🤑Assertion Passed: ${actual} === ${expected}`); {
-        if (actual !== expected) {
-          console.log(`🥵️Assertion Failed: ${actual} !== ${expected}`);
-        }
-      }
-    }
-  };
+const assertEqual = require('./assertEqual');
   
   const eqArrays = function(array1,array2) {
     if (array1.length !== array2.length) {
       return false;
     }
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
+      if (array1[i] === array2[i]) {
+        return true;
       }
     }
-    return true;
+    return false;
   };
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-  // Test Code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
