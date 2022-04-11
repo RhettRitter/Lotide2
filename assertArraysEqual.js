6b3bd4f16774d0105ef0a87cd60718b12dd00437
Rhett Ritter <rhett.ritter@hotmail.com>
2022-04-11 16:06:47 +0000

    modularized assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,9 @@
-const assertArraysEqual = function (arr1, arr2) {
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`🤑 ${arr1} === ${arr2}`)
+    console.log(`🤑🤑🤑 ${arr1} === ${arr2}`);
   } else {
-    console.log(`🥵️ ${arr1} !== ${arr2}`)
+    console.log(`🥵️🥵️🥵️ ${arr1} !== ${arr2}`);
   }
-}
+};
+module.exports = assertArraysEqual
